75191adb3b388896a9303c2bc5d39ad17b6027b0
Szász Kinga <kingus77@gmail.com>
2018-02-26 14:40:53 +0100

    basic html and css done

--- js/got.js
+++ js/got.js
@@ -22,10 +22,39 @@ function successAjax(xhttp) {
       A userDatas NEM GLOBÁLIS változó, ne is tegyétek ki globálisra. Azaz TILOS!
       Ha valemelyik függvényeteknek kell, akkor paraméterként adjátok át.
     */
+
+
 }
 
+
+function generateRow(objElement, arrElement) {
+    var td = document.createElement('td');
+    var element = objElement[arrElement];
+    td.textContent = element;
+    return td;
+}
+
+
+function createTable(userData, props) {
+    removeTable(document.getElementById('table'));
+    document.querySelector('#table').innerHTML = '';
+    var table = document.createElement('table');
+    table.appendChild(generateHead(head));
+    for (var i = 0; i < userData.length; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < props.length; j++) {
+            tr.appendChild(generateRow(userData[i], props[j]));
+            table.appendChild(tr);
+        }
+        document.querySelector('#table').appendChild(table);
+    }
+}
+
+
+
+
 // Írd be a json fileod nevét/útvonalát úgy, ahogy nálad van
-getData('/json/aJsonFileodNeve.json', successAjax);
+getData('json/characters.json', successAjax);
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
